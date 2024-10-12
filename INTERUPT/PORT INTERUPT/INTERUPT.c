6a66f4f24d4e23e5ed0e2db986612479dfdfac6a
Sridhar <144086400+sridhar1229@users.noreply.github.com>
2024-10-12 18:43:07 +0530

    Add files via upload

---
+++ INTERUPT/PORT INTERUPT/INTERUPT.c
@@ -0,0 +1,45 @@
+#pragma config FOSC = EXTRC     // Oscillator Selection bits (RC oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR disabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+
+#include <xc.h>
+#define _XTAL_FREQ 6000000
+
+unsigned char v;
+void main(void) {
+    TRISB =0xF0;
+    TRISC = 0x00;
+    TRISD = 0x00;
+    PORTB = 0x00;
+    PORTC = 0x00;
+    PORTD= 0x00;
+    OPTION_REG = 0x7F;
+    INTCON = 0x88;// enable global interupt and port interupt enabler
+    while(1)
+    {
+        PORTC = ~PORTC;
+        __delay_ms(500);
+    }
+        
+    return;
+}
+
+void __interrupt() ISR(void)
+{
+    v =PORTB;
+    if(INTCON & 0x01) // check for RBF (port interupt flag)
+    {
+    if(v == 0xE0)
+    {
+        PORTD = ~PORTD;
+    }
+    }
+    INTCON &= (~0x01);  //clear the flag  
+}
+    
